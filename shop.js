c43afd913de046318bda7e7a423dfc747841a740
Michael <fauxpawsc@live.com>
2015-11-15 02:11:19 -0600

    add working object methods

--- shop.js
+++ shop.js
@@ -1,13 +1,40 @@
+
+
 function Shop(location, minCustHr, maxCustHr, avgDonutsCust){
 	this.location = location;
 	this.minCustHr = minCustHr;
 	this.maxCustHr = maxCustHr;
 	this.avgDonutsCust = avgDonutsCust;
+	this.hoursOpen = 12;
+	this.hourlySold = [];
+	this.counter;
+	this.dayTotal = dayTotal;
+	
+
 
 	this.customers = function (){
+		 return Math.floor(Math.random() * (maxCustHr - minCustHr + 1)) + minCustHr;
+	
+	};
+	this.donutsBought = function (){
+		
+		return this.customers() * this.avgDonutsCust;
+	};
+	this.hourlyTotal = function (){
+		this.counter = 0;
+		this.dayTotal=0;
+		
+		for (i = 0; i < this.hoursOpen; i++){
+			this.counter = this.donutsBought();
+			this.dayTotal += this.counter;
+			this.hourlySold.push(this.counter);
+		};
 
 	}
-
+	this.hoursChange = function (newHours){
+		
+		
+	};
 }
 
 var downtown = new Shop('Downtown', 8, 43, 4.5);
@@ -21,3 +48,9 @@ console.log(capHill);
 console.log(southLU);
 console.log(wedge);
 console.log(bal);
+
+console.log(downtown.customers());
+console.log(downtown.donutsBought());
+console.log(downtown.hourlyTotal());
+console.log(downtown.hourlySold);
+console.log(downtown.dayTotal);
