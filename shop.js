c22c90b569b01e326817c86527f94cf3e63b401f
Michael <fauxpawsc@live.com>
2015-11-18 03:48:22 -0600

    add create location button that works

--- shop.js
+++ shop.js
@@ -1,4 +1,4 @@
-
+var create = document.getElementById('formstruct');
 
 function Shop(locationz, minCustHr, maxCustHr, avgDonutsCust){
 	this.locationz = locationz;
@@ -12,13 +12,14 @@ function Shop(locationz, minCustHr, maxCustHr, avgDonutsCust){
 	
 
 
+
 	this.customers = function (){
-		 return Math.floor(Math.random() * (maxCustHr - minCustHr + 1)) + minCustHr;
+		 return Math.random() * (maxCustHr - minCustHr + 1) + minCustHr;
 	
 	};
 	this.donutsBought = function (){
 		
-		return this.customers() * this.avgDonutsCust;
+		return Math.round(this.customers() * this.avgDonutsCust);
 	};
 	this.hourlyTotal = function (){
 		this.counter = 0;
@@ -35,13 +36,15 @@ function Shop(locationz, minCustHr, maxCustHr, avgDonutsCust){
 		
 		
 	};
+}
 
-	Shop.prototype.render = function(){
-		console.log(this.dayTotal);
-		var row = document.createElement('tr');
-		var data = document.createElement('th');
-		data.textContent = this.locationz;
-		row.appendChild(data);
+
+Shop.prototype.render = function(){
+	console.log(this.dayTotal);
+	var row = document.createElement('tr');
+	var data = document.createElement('th');
+	data.textContent = this.locationz;
+	row.appendChild(data);
 
 		for(i=0; i < this.hoursOpen; i++){
 			data = document.createElement('td');
@@ -49,42 +52,56 @@ function Shop(locationz, minCustHr, maxCustHr, avgDonutsCust){
 			row.appendChild(data);
 		}
 
-		data = document.createElement('th');
-		data.textContent = this.dayTotal;
-		row.appendChild(data);
+	data = document.createElement('th');
+	data.textContent = this.dayTotal;
+	row.appendChild(data);
 
-document.getElementById('tbody').appendChild(row);
-	};
+	document.getElementById('tbody').appendChild(row);
+};
+
+var submitNewShop = function(event){
+	event.preventDefault();
+	
+
+	if (!event.target.shop.value || !event.target.minCustHr.value || !event.target.maxCustHr.value || !event.target.avgDonutsCust.value) {
+		 alert('Fields cannot be empty!');
+
+	}
+	else {
+		var locale = event.target.shop.value;
+		var min = event.target.minCustHr.value;
+		var max = event.target.maxCustHr.value;
+		var donPerCust = event.target.avgDonutsCust.value;
+
+		var newShop = new Shop(locale, min, max, donPerCust);
+		allshops.push(newShop);
+		newShop.hourlyTotal();
+		newShop.render();
+		event.target.shop.value = null;
+		event.target.minCustHr.value = null;
+		event.target.maxCustHr.value = null;
+		event.target.avgDonutsCust.value = null;
+	}
+
+};
+
+var clearForm = function(event){
+
 }
 
-
-
-	var downtown = new Shop('Downtown', 8, 43, 4.5);
-	var capHill = new Shop('Capitol Hill', 4, 37, 2);
-	var southLU = new Shop('South Lake Union', 9, 23, 6.33);
-	var wedge = new Shop('Wedgewood', 2, 28, 1.25);
-	var bal = new Shop('Ballard', 8, 58, 3.75);
-
-
-
+var downtown = new Shop('Downtown', 8, 43, 4.5);
+var capHill = new Shop('Capitol Hill', 4, 37, 2);
+var southLU = new Shop('South Lake Union', 9, 23, 6.33);
+var wedge = new Shop('Wedgewood', 2, 28, 1.25);
+var bal = new Shop('Ballard', 8, 58, 3.75);
 var allshops = [downtown, capHill, southLU, wedge, bal];
 
+create.addEventListener('submit', submitNewShop);
 
-
-
-
-
-downtown.hourlyTotal();
-capHill.hourlyTotal();
-southLU.hourlyTotal();
-wedge.hourlyTotal();
-bal.hourlyTotal();
-
-downtown.render();
-capHill.render();
-southLU.render();
-wedge.render();
-bal.render();
+allshops.forEach(function(place){
+	place.hourlyTotal();
+	place.render();
+});
 
 
 
